15c21d4c19ca1929a720d91fad96d71a2d5154cd
Anbarasan <46339443+AnbarasanJ24@users.noreply.github.com>
2021-05-25 09:15:31 +0530

    Add user Authentication
    Handling user authentication & authorization using spotify API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import { getRefreshToken } from './Helper/spotify';
 import Login from './Login/Login';
+import React, { useEffect, useState } from 'react'
+// import SpotifyWebApi from 'spotify-web-api-js'
+
+// const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getRefreshToken();
+    const _token = hash.access_token;
+    window.location.hash = ""
+
+    if (_token) {
+      setToken(_token);
+      fetch("https://api.spotify.com/v1/me", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${_token}`
+        }
+      })
+        .then(response => response.json())
+        .then(user => {
+          console.log("user", user)
+        })
+        .catch(err => console.log(err))
+    }
+  }, [])
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? <h1>Logged In</h1> :
+          (
+            <Login />
+          )
+      }
     </div>
   );
 }
